0487bff1cc1d546489ee61304589e24ce6fbc954
Gaurav Kumar <gaurav.kumar@aeologic.com>
2022-07-25 16:12:48 +0530

    2 screen, add validator

--- lib/core/onbording/intro_screen.dart
+++ lib/core/onbording/intro_screen.dart
@@ -18,67 +18,65 @@ class IntroScreen extends StatefulWidget {
 class _IntroScreenState extends State<IntroScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-            child: Stack(
-              children: [
-                Container(
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      SizedBox(height: 551),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 70),
-                        child: Text(
-                            StringConstant.whatEver,
-                            textAlign: TextAlign.center,
-                            style: TextStyles.boldText(
-                              fontWeight: FontWeight.w500,
-                              color: ColorConstant.textColor
-                            )),
-                      ),
-                      SizedBox(height: 29),
-                      Container(
-                        height: 58,
-                        width: 311,
-                        decoration: BoxDecoration(
-                          color: ColorConstant.buttonColor,
-                          borderRadius: BorderRadius.circular(30)
-                        ),
-                        child: Center(
-                          child: Text(StringConstant.createAccount,
-                          style: TextStyles.boldText()
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 25),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const SignInScreen()));
-                        },
-                        child: Text(StringConstant.signIn,
-                          style: TextStyles.boldText(color: ColorConstant.navyBlue)),
-                      )
-                    ],
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Column(
+                children: [
+                  const SizedBox(height: 551),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 70),
+                    child: Text(
+                        StringConstant.whatEver,
+                        textAlign: TextAlign.center,
+                        style: TextStyles.boldText(
+                          fontWeight: FontWeight.w500,
+                          color: ColorConstant.textColor
+                        )),
                   ),
-                ),
-                SizedBox(height: 498,width: double.infinity,
-                  child: Image.asset(ImageConstant.backGround,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
+                  const SizedBox(height: 29),
+                  Container(
+                    height: 58,
+                    width: 311,
+                    decoration: BoxDecoration(
+                      color: ColorConstant.buttonColor,
+                      borderRadius: BorderRadius.circular(30)
+                    ),
+                    child: Center(
+                      child: Text(StringConstant.createAccount,
+                      style: TextStyles.boldText()
+                      ),
+                    ),
                   ),
-                ),
-
-                Positioned(
-                    top: 210,
-                    left: -37,
-                    child: Image.asset(ImageConstant.buddie,
-                      height: 253,
-                      width: 450,
-                    ))
-              ],
+                  const SizedBox(height: 17),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const SignInScreen()));
+                    },
+                    child: Text(StringConstant.signIn,
+                      style: TextStyles.boldText(color: ColorConstant.navyBlue)),
+                  )
+                ],
+              ),
             ),
+            SizedBox(height: 498,width: double.infinity,
+              child: Image.asset(ImageConstant.backGround,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+    
+            Positioned(
+                top: 210,
+                left: -37,
+                child: Image.asset(ImageConstant.buddie,
+                  height: 253,
+                  width: 450,
+                ))
+          ],
         ),
       ),
     );
--- lib/core/ui/signin/signin_screen.dart
+++ lib/core/ui/signin/signin_screen.dart
@@ -21,175 +21,178 @@ class _SignInScreenState extends State<SignInScreen> {
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32,vertical: 48),
-              child: Form(key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        InkWell(onTap: () {
-                          Navigator.pop(context);
-                        },
-                          child: Image.asset(ImageConstant.shape,
-                          color: ColorConstant.buttonBorderColor,
-                          ),
-                        ),
-                        SizedBox()
-                      ],
-                    ),
-                    SizedBox(height: 25),
-                    Center(
-                      child: Text(StringConstant.helloAgain,
-                      style: TextStyles.semiBoldText()
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Center(
-                      child: Text(StringConstant.signInToYourAccount,
-                      style: TextStyles.mediumText()
-                      ),
-                    ),
-                    SizedBox(height: 34),
-                    Text(StringConstant.emailAddress,
-                    style: TextStyles.regularText()
-                    ),
-                    SizedBox(height: 8),
-                    SizedBox(height: 48, width: 311,
-                      child: TextFormField(
-                        validator: Validator.validateUserEmailAddress,
-                        decoration: InputDecoration(
-                          hintText: StringConstant.gmailFormat,
-                          hintStyle: TextStyles.regularText(color: ColorConstant.gray),
-                          filled: true,
-                          fillColor: ColorConstant.transparent,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(color: ColorConstant.textFieldBorderColor)
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 21),
-                    Text(StringConstant.password,
-                        style: TextStyles.regularText()
-                    ),
-                    SizedBox(height: 8),
-                    SizedBox(height: 48, width: 311,
-                      child: TextFormField(
-                        validator: Validator.validatePasswordAddress,
-                        decoration: InputDecoration(
-                          hintText: StringConstant.passwordFormat,
-                          hintStyle: TextStyles.regularText(color: ColorConstant.gray),
-                          filled: true,
-                          fillColor: ColorConstant.transparent,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(color: ColorConstant.textFieldBorderColor)
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 21),
-                    Row(
-                      children: [
-                        SizedBox(),
-                        Spacer(),
-                        Text(StringConstant.forgetPassword,
-                        style: TextStyles.lightText(isUnderline: true),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 24),
-                    InkWell(
-                      onTap: () {
-                        if(_formKey.currentState!.validate()){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpAbout()));
-                        }
+    return SafeArea(
+      child: Scaffold(
+        
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32,vertical: 48),
+            child: Form(key: _formKey,
+              child: Column(
+                
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      InkWell(onTap: () {
+                        Navigator.pop(context);
                       },
-                      child: Container(
-                        height: 58,width: 311,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: ColorConstant.buttonColor
-                        ),
-                        child: Center(
-                          child: Text(StringConstant.signIn,
-                          style: TextStyles.boldText()),
+                        child: Image.asset(ImageConstant.shape,
+                        color: ColorConstant.buttonBorderColor,
                         ),
                       ),
+                      const SizedBox()
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  Center(
+                    child: Text(StringConstant.helloAgain,
+                    style: TextStyles.semiBoldText()
                     ),
-                    SizedBox(height: 21),
-                    Center(
-                      child: Text('..................................  Or with  .......................................',
-                      style: TextStyles.lightText(),
+                  ),
+                  const SizedBox(height: 8),
+                  Center(
+                    child: Text(StringConstant.signInToYourAccount,
+                    style: TextStyles.mediumText()
+                    ),
+                  ),
+                  const SizedBox(height: 34),
+                  Text(StringConstant.emailAddress,
+                  style: TextStyles.regularText()
+                  ),
+                  const SizedBox(height: 8),
+                  TextFormField(
+                   
+                    validator: Validator.validateUserEmailAddress,
+                    decoration: InputDecoration(
+                       contentPadding: EdgeInsets.fromLTRB(15, 12, 50, 12),
+
+                      hintText: StringConstant.gmailFormat,
+                      hintStyle: TextStyles.regularText(color: ColorConstant.gray,fontSize: 16),
+                      filled: true,
+                      fillColor: ColorConstant.transparent,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: ColorConstant.textFieldBorderColor)
                       ),
                     ),
-                    SizedBox(height: 24),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => WebViewScreen(url: 'https://www.google.co.in/')));
-                      },
-                      child: Container(
-                        height: 58,width: 311,
-                        decoration: BoxDecoration(
+                  ),
+                  const SizedBox(height: 21),
+                  Text(StringConstant.password,
+                      style: TextStyles.regularText()
+                  ),
+                 const SizedBox(height: 8),
+                  TextFormField(
+                    validator: Validator.validatePasswordAddress,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(15, 12, 50, 12),
+                      hintText: StringConstant.passwordFormat,
+                      hintStyle: TextStyles.regularText(color: ColorConstant.gray),
+                      filled: true,
+                      fillColor: ColorConstant.transparent,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: ColorConstant.textFieldBorderColor)
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 21),
+                  Row(
+                    children: [
+                      const SizedBox(),
+                      const Spacer(),
+                      Text(StringConstant.forgetPassword,
+                      style: TextStyles.lightText(isUnderline: true),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 24),
+                  InkWell(
+                    onTap: () {
+                      if(_formKey.currentState!.validate()){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpAbout()));
+                      }
+                    },
+                    child: Container(
+                      height: 58,width: 311,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: ColorConstant.buttonColor
+                      ),
+                      child: Center(
+                        child: Text(StringConstant.signIn,
+                        style: TextStyles.boldText()),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 21),
+                  Center(
+                    child: Text('..................................  Or with  .......................................',
+                    style: TextStyles.lightText(),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const WebViewScreen(url: 'https://www.google.co.in/')));
+                    },
+                    child: Container(
+                      height: 58,width: 311,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: ColorConstant.buttonBorderColor),
+                        color: ColorConstant.transparent
+                      ),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(ImageConstant.google,height: 24,width: 24,fit: BoxFit.cover,),
+                          const SizedBox(width: 8),
+                          Text(StringConstant.signWithGoogle,
+                          style: TextStyles.semiBoldText(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const WebViewScreen(url: 'https://www.facebook.com/',)));
+                    },
+                    child: Container(
+                      height: 58,width: 311,
+                      decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(30),
                           border: Border.all(color: ColorConstant.buttonBorderColor),
                           color: ColorConstant.transparent
-                        ),
-                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(ImageConstant.google,height: 24,width: 24,fit: BoxFit.cover,),
-                            SizedBox(width: 8),
-                            Text(StringConstant.signWithGoogle,
+                      ),
+                      child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(ImageConstant.facebook,height: 24,width: 24,fit: BoxFit.cover,),
+                          const SizedBox(width: 8),
+                          Text(StringConstant.signWithFacebook,
                             style: TextStyles.semiBoldText(fontSize: 16),
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(height: 16),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => WebViewScreen(url: 'https://www.facebook.com/',)));
-                      },
-                      child: Container(
-                        height: 58,width: 311,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(color: ColorConstant.buttonBorderColor),
-                            color: ColorConstant.transparent
-                        ),
-                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(ImageConstant.facebook,height: 24,width: 24,fit: BoxFit.cover,),
-                            SizedBox(width: 8),
-                            Text(StringConstant.signWithFacebook,
-                              style: TextStyles.semiBoldText(fontSize: 16),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 32),
-                    Row(mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(StringConstant.donNotHaveAccount,
-                        style: TextStyles.lightText(),),
-                        Text(StringConstant.signUp,
-                        style: TextStyles.lightText(color: ColorConstant.containerColor,isUnderline: true),
-                        )
-                      ],
-                    ),
-
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 32),
+                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(StringConstant.donNotHaveAccount,
+                      style: TextStyles.lightText(),),
+                      Text(StringConstant.signUp,
+                      style: TextStyles.lightText(color: ColorConstant.containerColor,isUnderline: true),
+                      )
+                    ],
+                  ),
+            
+                ],
               ),
-            )),
+            ),
+          ),
+        ),
       ),
     );
   }
